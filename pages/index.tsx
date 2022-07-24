5e51a1d80ce9a719f498867f761f74fcbfa1f8c4
yehudz <yehuda.zeytim@gmail.com>
2022-07-24 16:40:54 -0600

    Topbar pass is mobile prop

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,21 @@
 import type { NextPage } from 'next'
 import TopBar from '../components/layout/TopBar'
 import ColumnsContainer from '../components/layout/ColumnsContainer'
+import Head from 'next/head'
 const Home: NextPage = (props) => {
   return (
-    <div className="w-screen h-screen">
-      <TopBar />
-      <ColumnsContainer />
-    </div>
+    <>
+      <Head>
+        <title>Kanban Task Managment</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@500;700&display=swap" rel="stylesheet" />
+      </Head>
+      <div className="w-screen h-screen">
+        <TopBar isMobile={true}/>
+        <ColumnsContainer />
+      </div>
+    </>
   )
 }
 
