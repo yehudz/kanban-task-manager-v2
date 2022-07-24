a34173271cb5361cbd31ffe4b91d5896525c894f
yehudz <yehuda.zeytim@gmail.com>
2022-07-24 17:07:16 -0600

    refactor: Column title test
    Should only check for mobile drop down to show or not

--- cypress/component/ui/ColumnTitle.cy.tsx
+++ cypress/component/ui/ColumnTitle.cy.tsx
@@ -2,21 +2,20 @@
 import ColumnTitle from '../../../components/ui/ColumnTitle'
 describe('<ColumnTitle>', () => {
   
-  it('should mount column mobile title', () => {
-    cy.viewport(320,	568)
-    cy.mount(<ColumnTitle title={'Column Title'} isMobile={true}/>)
-    cy.get('[data-testid=column-title]').should('exist')
-    cy.get('[data-testid=column-drop-down-icon]').should('exist')
-  })
-
-  it('should have mobile logo container', () => {
-    cy.viewport(320,	568)
-    cy.mount(<ColumnTitle title={'Column Title'} isMobile={true}/>)
-    cy.get('[data-testid=column-title]').should('exist')
-    cy.get('[data-testid=column-drop-down-icon]').should('exist')
-  })
-
   it('should mount column title', () => {
+    cy.viewport(320,	568)
+    cy.mount(<ColumnTitle title={'Column Title'} isMobile={true}/>)
+    cy.get('[data-testid=column-title]').should('exist')
+    cy.get('[data-testid=column-drop-down-icon]').should('exist')
+  })
+
+  it('should show mobile drop down menu', () => {
+    cy.viewport(320,	568)
+    cy.mount(<ColumnTitle title={'Column Title'} isMobile={true}/>)
+    cy.get('[data-testid=column-drop-down-icon]').should('exist')
+  })
+
+  it('should not show drop down menu on desktop', () => {
     cy.viewport(1280,	800)
     cy.mount(<ColumnTitle title={'Column Title'} isMobile={false}/>)
     cy.get('[data-testid=column-title]').should('exist')
