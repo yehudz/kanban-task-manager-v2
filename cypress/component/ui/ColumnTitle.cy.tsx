f121e28b87578cd41833af4774b7cdd8d6c1cba4
yehudz <yehuda.zeytim@gmail.com>
2022-07-24 16:38:50 -0600

    Cypress tests update

---
+++ cypress/component/ui/ColumnTitle.cy.tsx
@@ -0,0 +1,26 @@
+
+import ColumnTitle from '../../../components/ui/ColumnTitle'
+import '../../styles/globals.css';
+describe('<ColumnTitle>', () => {
+  
+  it('should mount column mobile title', () => {
+    cy.viewport(320,	568)
+    cy.mount(<ColumnTitle title={'Column Title'} isMobile={true}/>)
+    cy.get('[data-testid=column-title]').should('exist')
+    cy.get('[data-testid=column-drop-down-icon]').should('exist')
+  })
+
+  it('should have mobile logo container', () => {
+    cy.viewport(320,	568)
+    cy.mount(<ColumnTitle title={'Column Title'} isMobile={true}/>)
+    cy.get('[data-testid=column-title]').should('exist')
+    cy.get('[data-testid=column-drop-down-icon]').should('exist')
+  })
+
+  it('should mount column title', () => {
+    cy.viewport(1280,	800)
+    cy.mount(<ColumnTitle title={'Column Title'} isMobile={false}/>)
+    cy.get('[data-testid=column-title]').should('exist')
+    cy.get('[data-testid=column-drop-down-icon]').should('not.exist')
+  })
+})
--- cypress/e2e/mobile-layout-test.cy.ts
+++ cypress/e2e/mobile-layout-test.cy.ts
@@ -4,11 +4,27 @@ describe('empty spec', () => {
     cy.viewport(320,	568)
   })
 
-  it('checks if desktop topbar exists', () => {
+  it('checks if topbar exists', () => {
     cy.get('[data-testid=top-bar]').should('exist')
   })
 
   it('checks if there is a columns cointainer', ()=> {
     cy.get('[data-testid=columns-container]').should('exist')
+  })
+
+  it('checks if there is a column title', ()=> {
+    cy.get('[data-testid=column-title]').should('exist')
+  })
+
+  it('checks if there is a logo container', ()=> {
+    cy.get('[data-testid=logo-container]').should('exist')
+  })
+
+  it('checks if there is an add new task button', ()=> {
+    cy.get('[data-testid=add-new-task-button]').should('exist')
+  })
+
+  it('checks if there is an options button for column', ()=> {
+    cy.get('[data-testid=column-options-drop-down]').should('exist')
   })
 })
