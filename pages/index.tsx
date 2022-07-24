714349fce29dc83b9ba9d8712e17ddb97868137f
yehudz <yehuda.zeytim@gmail.com>
2022-07-24 15:16:07 -0600

    Dark and light theme setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = (props) => {
   return (
-    <div className={styles.container}>
-      <h1 className="text-3xl font-bold underline">
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <h1 className="text-4xl font-bold">
         Hello world!
       </h1>
     </div>
