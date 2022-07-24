9accddfdd2c6ee1a3c78c3df8ff8ce2db6627453
yehudz <yehuda.zeytim@gmail.com>
2022-07-24 16:39:12 -0600

    Add Top bar component

--- components/layout/TopBar.tsx
+++ components/layout/TopBar.tsx
@@ -1,8 +1,17 @@
 import styles from '../../styles/layout/Topbar.module.scss'
+import ColumnTitle from '../ui/ColumnTitle'
+import MobileLogo from '../ui/MobileLogo'
 
-const TopBar = ()=> {
+interface TopBarProps {
+  isMobile: boolean
+}
+
+const TopBar = ({isMobile}: TopBarProps)=> {
   return(
-    <div data-testid="top-bar" className={`${styles.container} w-screen`}></div>
+    <div data-testid="top-bar" className={`${styles.container} w-screen`}>
+      <MobileLogo show={isMobile ? true : false}/>
+      <ColumnTitle title={'Example Title'} isMobile={false}/>
+    </div>
   )
 }
 
