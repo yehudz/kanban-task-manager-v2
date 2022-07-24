5863579738c85e3bfcaeda1cc25ea7f2798dc40c
yehudz <yehuda.zeytim@gmail.com>
2022-07-24 16:38:33 -0600

    Cypress and tailwind updated config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,22 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#635FC7',
+      'purple-light': '#A8A4FF',
+      'black': '000112',
+      'midnight': '#20212C',
+      'grey': '#2B2C37',
+      'grey-700': '#3E3F4E',
+      'grey-400': '#828FA3',
+      'grey-200': '#E4EBFA',
+      'grey-100': '#F4F7FD',
+      'red': '#EA5555',
+      'red-light': '#FF9898'
+    },
   },
   plugins: [],
 }
