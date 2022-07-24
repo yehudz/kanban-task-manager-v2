c8592fd47c1df748ac91bde2bc63ce08ecdce558
yehudz <yehuda.zeytim@gmail.com>
2022-07-24 14:57:06 -0600

    Tailwind setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
 
-const Home: NextPage = () => {
+const Home: NextPage = (props) => {
   return (
     <div className={styles.container}>
-      
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </div>
   )
 }
