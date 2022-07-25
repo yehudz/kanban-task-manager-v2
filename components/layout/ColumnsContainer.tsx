71efb53d7c03b4a028a072bbbdda304f7ca3e31b
yehudz <yehuda.zeytim@gmail.com>
2022-07-25 11:11:59 -0600

    refactor: Name change

--- components/layout/ColumnsContainer.tsx
+++
@@ -1,9 +0,0 @@
-import styles from '../../styles/layout/ColumnsContainer.module.scss'
-
-const ColumnsContainer = ()=> {
-  return(
-    <div data-testid="columns-container" className={`${styles.container} w-screen h-screen`}></div>
-  )
-}
-
-export default ColumnsContainer
