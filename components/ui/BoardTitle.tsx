c2dec93f451c08b8587570752b84392b0bca9a56
yehudz <yehuda.zeytim@gmail.com>
2022-07-25 16:08:13 -0600

    refactor: Rename testing
    Column title should be board title

--- components/ui/BoardTitle.tsx
+++ components/ui/BoardTitle.tsx
@@ -3,8 +3,8 @@ import { BoardTitleProps } from '../../typings/interfaces';
 const BoardTitle = ({title, isMobile, handleClick}: BoardTitleProps)=> {
   return(
     <div onClick={handleClick}>
-      <h1 data-testid="column-title">{title}</h1>
-      {isMobile && <div data-testid="column-drop-down-icon">V</div>}
+      <h1 data-testid="board-title">{title}</h1>
+      {isMobile && <div data-testid="board-drop-down-icon">V</div>}
     </div>
   )
 }
--- cypress/component/ui/BoardTitle.cy.tsx
+++ cypress/component/ui/BoardTitle.cy.tsx
@@ -5,20 +5,20 @@ describe('<BoardTitle>', () => {
   it('should mount column title', () => {
     cy.viewport(320,	568)
     cy.mount(<BoardTitle title={'Column Title'} isMobile={true} handleClick={()=> {}}/>)
-    cy.get('[data-testid=column-title]').should('exist')
-    cy.get('[data-testid=column-drop-down-icon]').should('exist')
+    cy.get('[data-testid=board-title]').should('exist')
+    cy.get('[data-testid=board-drop-down-icon]').should('exist')
   })
 
   it('should show mobile drop down menu', () => {
     cy.viewport(320,	568)
     cy.mount(<BoardTitle title={'Column Title'} isMobile={true} handleClick={()=> {}}/>)
-    cy.get('[data-testid=column-drop-down-icon]').should('exist')
+    cy.get('[data-testid=board-drop-down-icon]').should('exist')
   })
 
   it('should not show drop down menu on desktop', () => {
     cy.viewport(1280,	800)
     cy.mount(<BoardTitle title={'Column Title'} isMobile={false} handleClick={()=> {}}/>)
-    cy.get('[data-testid=column-title]').should('exist')
-    cy.get('[data-testid=column-drop-down-icon]').should('not.exist')
+    cy.get('[data-testid=board-title]').should('exist')
+    cy.get('[data-testid=board-drop-down-icon]').should('not.exist')
   })
 })
