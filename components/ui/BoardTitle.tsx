4ea170c5dd7ae894255669eb0748d00185a831fe
yehudz <yehuda.zeytim@gmail.com>
2022-07-25 16:04:54 -0600

    refactor: Remove used code

--- components/ui/BoardTitle.tsx
+++ components/ui/BoardTitle.tsx
@@ -1,13 +1,6 @@
-import {useContext} from 'react';
-import appContext from '../../context/appContext'
 import { BoardTitleProps } from '../../typings/interfaces';
 
 const BoardTitle = ({title, isMobile, handleClick}: BoardTitleProps)=> {
-  // const {setOpenMobileMenu} = useContext(appContext)
-
-  // function handleMobileMenuVisibility() {
-  //   if (isMobile) handleClick((prevState: boolean)=> prevState = !prevState)
-  // }
   return(
     <div onClick={handleClick}>
       <h1 data-testid="column-title">{title}</h1>
