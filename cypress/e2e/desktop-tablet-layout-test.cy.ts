7908fb5dfc0873daa9234c406c1b3830a9ff67ec
yehudz <yehuda.zeytim@gmail.com>
2022-07-24 19:22:30 -0600

    update: Add desktop layout testing

---
+++ cypress/e2e/desktop-tablet-layout-test.cy.ts
@@ -0,0 +1,30 @@
+describe('empty spec', () => {
+  beforeEach(()=> {
+    cy.visit('/')
+    cy.viewport(1280,	800)
+  })
+
+  it('checks if topbar exists', () => {
+    cy.get('[data-testid=top-bar]').should('exist')
+  })
+
+  it('checks if there is a columns cointainer', ()=> {
+    cy.get('[data-testid=columns-container]').should('exist')
+  })
+
+  it('checks if there is a column title', ()=> {
+    cy.get('[data-testid=column-title]').should('exist')
+  })
+
+  it('checks if there is a logo container', ()=> {
+    cy.get('[data-testid=logo-container]').should('not.exist')
+  })
+
+  it('checks if there is an add new task button', ()=> {
+    cy.get('[data-testid=add-new-task-button]').should('exist')
+  })
+
+  it('checks if there is an options button for column', ()=> {
+    cy.get('[data-testid=column-options-drop-down]').should('exist')
+  })
+})
