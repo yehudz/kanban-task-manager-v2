055cacd442d6db9f7f97fb73c93abb42463f03ad
yehudz <yehuda.zeytim@gmail.com>
2022-07-26 17:04:31 -0600

    style: Sidebar logo

--- components/layout/RightSidebar.tsx
+++ components/layout/RightSidebar.tsx
@@ -2,6 +2,9 @@ import { useState } from "react"
 import ColumnsListMenu from "../ui/ColumnsListMenu"
 import ThemeToggle from "../ui/ThemeToggle"
 import styles from '../../styles/layout/RightSidebar.module.scss'
+import logoDark from '../../public/assets/logo-dark.svg'
+import logoLight from '../../public/assets/logo-light.svg'
+
 
 const RightSidebar = ()=> {
   const [sidebarOpen, setSidebarOpen] = useState<boolean>(true)
@@ -13,7 +16,9 @@ const RightSidebar = ()=> {
     <div 
       data-testid="right-sidebar" 
       className={`${styles.container} ${sidebarOpen ? 'sidebarOpen' : ''} h-full bg-white dark:bg-midnight relative`}>
-      <div data-testid="right-sidebar-logo"></div>
+      <div data-testid="right-sidebar-logo" className={`${styles.logoContainer} flex items-center w-full justify-center`}>
+        <img src="images/logo-light.svg" alt="" className={`${styles.image}`}/>
+      </div>
       <ColumnsListMenu />
       <ThemeToggle />
       <div data-testid="toggle-sidebar-button" onClick={toggleSidebarVisibility}>Hide Sidebar</div>
