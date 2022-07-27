815cd45e76ea6e12c525c1c62efc80dc5ea9e067
yehudz <yehuda.zeytim@gmail.com>
2022-07-26 21:37:22 -0600

    fix: Mobile layout failed to open mobile menu

--- cypress/e2e/mobile-layout-test.cy.ts
+++ cypress/e2e/mobile-layout-test.cy.ts
@@ -30,7 +30,7 @@ describe('Mobile Layout Testing', () => {
 
   it('opens the mobile menu and have list items', ()=> {
     cy.get('[data-testid=mobile-menu]').should('exist')
-    cy.get('[data-testid=board-title]').click()
+    cy.get('[data-testid=board-title]').click({force: true})
     cy.get('[data-testid=mobile-menu]').should('not.have.class', 'hidden')
 
     cy.get('[data-testid=board-list-items]').should('have.length.at.least', 1)
