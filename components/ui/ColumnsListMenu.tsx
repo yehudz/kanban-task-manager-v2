727adfa2e0570d4c4a69d28b02f9c5241f7e440f
yehudz <yehuda.zeytim@gmail.com>
2022-07-26 18:02:55 -0600

    style: Multiple elements - Check commit body
    Hover state of boards list in sidebar
    Create new board button styling

--- components/ui/ColumnsListMenu.tsx
+++ components/ui/ColumnsListMenu.tsx
@@ -17,7 +17,7 @@ const ColumnsListMenu = ()=> {
   return(
     <div data-testid="columns-list-menu" className={`${styles.container} w-full`}>
       <div data-testid="columns-list-menu-title" className={`${styles.title} text-grey-400 font-bold`}>All Boards (8)</div>
-      <div data-testid="board-list-items" className="my-8">
+      <div data-testid="board-list-items" className="mt-7">
         {boardsDummyData.map((item: BoardListItem)=> {
           return(
             <div className="flex items-center">
@@ -26,7 +26,10 @@ const ColumnsListMenu = ()=> {
           )
         })}
       </div>
-      <div onClick={()=> showAddBoardForm()} data-testid="add-board-button">Create New Board</div>
+      <div onClick={()=> showAddBoardForm()} data-testid="add-board-button" className={`flex flex-row items-center cursor-pointer text-purple ${styles.createButton}`}>
+        <img src="images/icon-board.svg" alt="board" />
+        <span>+ Create New Board</span>  
+      </div>
     </div>
   )
 }
