d833404d1cf5889528ac2f96d78d6b22b263ad22
yehudz <yehuda.zeytim@gmail.com>
2022-07-27 20:28:34 -0600

    update: Add new column UI styles

--- components/ui/AddNewColumnUi.tsx
+++ components/ui/AddNewColumnUi.tsx
@@ -1,6 +1,10 @@
+import styles from '../../styles/ui/AddNewColumnUi.module.scss'
+
 const AddNewColumnUi = ()=> {
   return(
-    <div data-testid="add-new-column-ui"> Add new column</div>
+    <div data-testid="add-new-column-ui" className={`${styles.container} flex items-center justify-center h-sreen bg-grey-200 dark:bg-grey bg-opacity-60 dark:bg-opacity-20 rounded-lg cursor-pointer`}>
+      <h2 className={`${styles.title} text-grey-400`}>+ New column</h2>
+    </div>
   )
 }
 
