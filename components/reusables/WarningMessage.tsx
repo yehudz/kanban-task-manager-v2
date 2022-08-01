73c2723009a1276dfef660c418f05ca43bfed720
yehudz <yehuda.zeytim@gmail.com>
2022-08-01 17:38:30 -0600

    style: Warning message styling

--- components/reusables/WarningMessage.tsx
+++ components/reusables/WarningMessage.tsx
@@ -17,22 +17,35 @@ const WarningMessage = ({title, itemName, type}: WarningMessageProps)=> {
   const Message = ()=> {
     if (type === 'task') {
       return(
-        <p data-testid="warning-message-tex">Are you sure you want to delete the '{itemName}' task and its subtasks? This action cannot be reversed.</p>
+        <p data-testid="warning-message-tex" className="text-grey-400 mb-6">Are you sure you want to delete the '{itemName}' task and its subtasks? This action cannot be reversed.</p>
       )
     } else {
       return(
-        <p data-testid="warning-message-tex">Are you sure you want to delete the '{itemName}' board? This action will remove all columns and tasks and cannot be reversed.</p>
+        <p data-testid="warning-message-tex" className="text-grey-400 mb-6">Are you sure you want to delete the '{itemName}' board? This action will remove all columns and tasks and cannot be reversed.</p>
       )
     }
   }
 
   return(
-    <div data-testid="warning-message-container" className={`${styles.container} bg-white dark:bg-grey p-6 rounded-lg`}>
-      <h1 data-testid="warning-message-title">{title}</h1>
+    <div data-testid="warning-message-container" className={`${styles.container} bg-white dark:bg-grey py-12 px-12 rounded-lg`}>
+      <h2 data-testid="warning-message-title" className="text-red mb-6">{title}</h2>
       <Message />
-      <div>
-        <button onClick={handleClick} data-testid="warning-message-confirm-button" type="button" className="py-3 w-52 rounded-full bg-red hover:bg-red-light">Confirm</button>
-        <button data-testid="warning-message-cancel-button" type="button" className="py-3 w-52 rounded-full bg-white hover:bg-white text-purple">Cancel</button>
+      <div className="flex flex-row items-center justify-between ">
+        <button 
+          onClick={handleClick} 
+          data-testid="warning-message-confirm-button" 
+          type="button" 
+          className="py-3 w-52 rounded-full bg-red hover:bg-red-light"
+          >
+            Delete
+          </button>
+        <button 
+          data-testid="warning-message-cancel-button" 
+          type="button" 
+          className="py-3 w-52 rounded-full bg-white hover:bg-white dark:bg-grey-100 dark:hover:bg-grey-200 text-purple"
+          >
+            Cancel
+          </button>
       </div>
     </div>
   )
