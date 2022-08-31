677d7be1af4668b3f56b000f9c8fbb01d56fe944
yehudz <yehuda.zeytim@gmail.com>
2022-08-31 13:10:48 -0600

    [KTMF-6] Use new API to get tasks
    Refactor: Renaming of fetch request to new API
    Stylized code for readability

--- components/reusables/BoardColumn.tsx
+++ components/reusables/BoardColumn.tsx
@@ -5,30 +5,57 @@ import styles from '../../styles/reusables/BoardColumn.module.scss'
 import { useContext, useEffect, useState } from "react"
 import { TaskItem } from '../../typings/common.types'
 import appContext from '../../context/appContext'
-const BoardColumn = ({id, name, color}: BoardColumnProps)=> {
-  const {newTaskCreated, setNewTaskCreated} = useContext(appContext)
+const BoardColumn = (
+  {
+    id, 
+    name, 
+    color
+  }: BoardColumnProps)=> {
+  const {
+      newTaskCreated, 
+      setNewTaskCreated
+    } = useContext(appContext)
   const [tasks, setTasks] = useState<TaskItem[]>([])
   async function getAllTasks() {
-    let getParams = {
-      columnId: id
-    }
-    let res = await fetch(`/api/getTasks`, {
-      method: "POST",
-      body: JSON.stringify(getParams)
+    let res = await fetch(`http://localhost:3001/api/v2/tasks/${id}`, {
+      method: "GET",
     })
 
     let result = await res.json()
-    setTasks(result.tasks)
+    setTasks(result)
     setNewTaskCreated(false)
   }
   useEffect(()=> {
     getAllTasks()
   }, [newTaskCreated])
   return(
-    <div data-testid="board-column-container" className={`${styles.container} h-full mb-3`}>
+    <div 
+      data-testid="board-column-container" 
+      className={`
+        ${styles.container} 
+        h-full 
+        mb-3
+      `
+    }>
       <div className="flex flex-row">
-        <div className={`${styles.badge} mr-4`} style={{backgroundColor: color}}></div>
-        <div data-testid="board-column-title" className={`${styles.title} text-grey-400 font-bold`}>{name} ({tasks?.length})</div>
+        <div 
+          className={`
+            ${styles.badge} 
+            mr-4
+          `} 
+          style={{backgroundColor: color}}
+        >
+        </div>
+        <div 
+          data-testid="board-column-title" 
+          className={`
+            ${styles.title} 
+            text-grey-400 
+            font-bold
+          `}
+        >
+          {name} ({tasks?.length})
+        </div>
       </div>
         {tasks && <div className="flex flex-col">
           {tasks?.map((task)=> {
@@ -38,8 +65,7 @@ const BoardColumn = ({id, name, color}: BoardColumnProps)=> {
                 id={task.id}
                 title={task.title}
                 description={task.description}
-                status={task.status}
-                subtasks={task.subtasks}
+                order={task.order}
               />
             )
           })}
