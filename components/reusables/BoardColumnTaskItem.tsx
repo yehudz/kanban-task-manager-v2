a23b3e4366b7add49849ba6c04e819877b1d85e7
yehudz <yehuda.zeytim@gmail.com>
2022-08-31 13:13:27 -0600

    [KTMF-6] Task Item refactor
    Use of subtask getter
    Stylistic refactor for code readability
    Sets subtasks as props for modal

--- components/reusables/BoardColumnTaskItem.tsx
+++ components/reusables/BoardColumnTaskItem.tsx
@@ -1,15 +1,32 @@
-import { useContext } from "react"
-import { TaskItem } from "../../typings/common.types"
+import { useContext, useEffect, useState } from "react"
+import type { Subtask, TaskItem } from "../../typings/common.types"
 import appContext from "../../context/appContext"
 import styles from '../../styles/reusables/BoardColumnTaskItem.module.scss'
-const BoardColumnTaskItem = ({id, title, description, status, subtasks}: TaskItem)=> {
-  const {setModalVisibility, setTaskDetails, setModalContentType} = useContext(appContext)
+import getSubtasks from "../hooks/getSubtasks"
+const BoardColumnTaskItem = (
+  {
+    id, 
+    title, 
+    description, 
+  }: TaskItem)=> {
+  const [subtasks, setSubtasks] = useState<Subtask[]>([])
+  const {
+    setModalVisibility, 
+    setTaskDetails, 
+    setModalContentType
+  } = useContext(appContext)
 
+  useEffect(()=> {
+    const fetchData = async () => {
+      setSubtasks(await getSubtasks(id))
+    };
+
+    fetchData();
+  }, [])
   let params = {
     id,
     title,
     description,
-    status,
     subtasks
   }
 
@@ -20,9 +37,47 @@ const BoardColumnTaskItem = ({id, title, description, status, subtasks}: TaskIte
   }
 
   return(
-    <div data-testid="task-item" onClick={showModal} className={`${styles.container} bg-white dark:bg-grey rounded-lg shadow-md`}>
-      <h3 data-testid="task-title" className="text-grey dark:text-white">{title}</h3>
-      <div data-testid="task-subtasks" className="body-m text-grey-400 mt-1">{subtasks?.filter(subtask=>subtask.isCompleted).length} of {subtasks?.length} substasks</div>
+    <div 
+      data-testid="task-item" 
+      onClick={showModal} 
+      className={`
+        ${styles.container} 
+        bg-white 
+        dark:bg-grey 
+        rounded-lg 
+        shadow-md
+      `}
+    >
+      <h3 
+        data-testid="task-title" 
+        className="
+          text-grey 
+          dark:text-white
+        "
+      >
+        {title}
+      </h3>
+      <div 
+        data-testid="task-subtasks" 
+        className="
+          body-m 
+          text-grey-400 
+          mt-1
+        "
+      >
+        <span>{
+            subtasks ?
+            subtasks.filter(subtask=>subtask.completed).length :
+            0
+          } </span>
+        <span>of </span>
+        <span>{
+            subtasks ? 
+            subtasks.length : 
+            0
+          } </span>
+        <span>subtasks</span>
+      </div>
     </div>
   )
 }
