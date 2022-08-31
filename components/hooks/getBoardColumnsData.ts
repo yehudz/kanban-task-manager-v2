1b7ed92a37f65b325c6e0a26b0deeeceec7e5bb4
yehudz <yehuda.zeytim@gmail.com>
2022-08-31 14:29:00 -0600

    [KTMF-6] Create hook to get board column data
    This uses basic data from board columns to show on the status dropdown.

---
+++ components/hooks/getBoardColumnsData.ts
@@ -0,0 +1,20 @@
+export default async function getBoardColumnsData(boardId: string) {
+  const boardColumns = await fetch(`
+    http://localhost:3001/api/v2/boardColumns/${boardId}
+  `)
+  const results = await boardColumns.json()
+
+  type DataNeeded = {
+    id: string,
+    name: string
+  }
+
+  const dataNeeded = results.map((result: DataNeeded)=> {
+    return {
+        id: result.id,
+        name: result.name
+    }
+  })
+
+  return dataNeeded
+}
