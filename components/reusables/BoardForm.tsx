aba1f91af15172e543ded5cd5404f09d5271bc95
yehudz <yehuda.zeytim@gmail.com>
2022-09-01 18:27:19 -0600

    Update BoardForm.tsx
    Stylistic refactor for code readability

--- components/reusables/BoardForm.tsx
+++ components/reusables/BoardForm.tsx
@@ -10,11 +10,33 @@ import Input from "../ui/Input";
 import { IconButton } from "@mui/material";
 
 const BoardForm = ({formTitle, boardName, boardColumns}: BoardFormProps)=> {
-  const {setModalVisibility, setNewCreatedBoard, boardId} = useContext(AppContext)
+  const {
+      setModalVisibility, 
+      setNewCreatedBoard, 
+      boardId
+    } = useContext(AppContext) 
 
-  const [boardNameValue, setBoardNameValue] = useState<string>(boardName? boardName : '')
-  const [emptyBoardColumns, setEmptyBoardColumns] = useState<BoardColumn[]>([{id: '', name: '', color: '', placeholder: 'e.g. To do', order: 1, tasks: []}])
-  const [boardColumnName, setBoardColumnName] = useState<string>('')
+  const [
+    boardNameValue, 
+    setBoardNameValue
+  ] = useState<string>(boardName? boardName : '')
+  const [
+    emptyBoardColumns, 
+    setEmptyBoardColumns
+  ] = useState<BoardColumn[]>([
+    {
+      id: '', 
+      name: '', 
+      color: '', 
+      placeholder: 'e.g. To do', 
+      order: 1, 
+      tasks: []
+    }
+  ])
+  const [
+    boardColumnName, 
+    setBoardColumnName
+  ] = useState<string>('')
   const [columns, setColumns] = useState<BoardColumn[]>([])
   const [createResource, setCreateBoard] = useState<boolean>(false)
   const [type, setType] = useState<string>('')
@@ -35,7 +57,7 @@ const BoardForm = ({formTitle, boardName, boardColumns}: BoardFormProps)=> {
 
   // Generates the params for API call
   useEffect(()=> {
-    // if (!boardNameValue || !boardColumnName) return // Returns if no values are set
+    //  Returns if no values are set
     if (createResource) {
       // this we only need when editing
       if (inputsContainer.current && type === 'edit') {
@@ -55,11 +77,27 @@ const BoardForm = ({formTitle, boardName, boardColumns}: BoardFormProps)=> {
   }, [createResource, columns])
 
   // Add new input field for new column
-  function addNewColumnField(event: React.MouseEvent<HTMLButtonElement>) {
+  function addNewColumnField(
+    event: React.MouseEvent<HTMLButtonElement>
+  ) {
     let color = randomColor()
     event.preventDefault()
-    setColumns(prevColumns=> [...prevColumns, {name: boardColumnName, order: prevColumns.length + 1, color: color}])
-    setEmptyBoardColumns(prevColumns=> [...prevColumns, {name: '', color: '', placeholder: 'e.g. Enter column title', order: 1, tasks: []}])
+    setColumns(prevColumns=> [...prevColumns, 
+      {
+        name: boardColumnName, 
+        order: prevColumns.length + 1, 
+        color: color
+      }
+    ])
+    setEmptyBoardColumns(prevColumns=> [...prevColumns, 
+      {
+        name: '', 
+        color: '', 
+        placeholder: 'e.g. Enter column title', 
+        order: 1, 
+        tasks: []
+      }
+    ])
     setBoardColumnName('')
   }
 
@@ -70,7 +108,13 @@ const BoardForm = ({formTitle, boardName, boardColumns}: BoardFormProps)=> {
     if (!boardNameValue || !boardColumnName) return // Returns if no values are set
     // This we need for when creating a new board
     let color = randomColor()
-    setColumns(prevColumns => [...prevColumns, {name: boardColumnName, order: prevColumns.length+1,color: color}])
+    setColumns(prevColumns => [...prevColumns, 
+      {
+        name: boardColumnName, 
+        order: prevColumns.length+1,
+        color: color
+      }
+    ])
   }
 
   // Makes API call to save the form to DB
@@ -88,9 +132,38 @@ const BoardForm = ({formTitle, boardName, boardColumns}: BoardFormProps)=> {
   }
 
   return(
-    <form data-testid="add-new-board-form" className={`${styles.container} flex flex-col bg-white dark:bg-grey pt-12 pb-8 px-12 rounded-lg`}>
-      <h2 data-testid="add-new-board-form-title" className="text-grey dark:text-white">{formTitle}</h2>
-      <span data-testid="add-new-board-form-title-input" className="flex flex-col mb-8 mt-8">
+    <form 
+      data-testid="add-new-board-form" 
+      className={`
+        ${styles.container} 
+        flex 
+        flex-col 
+        bg-white 
+        dark:bg-grey 
+        pt-12 
+        pb-8 
+        px-12 
+        rounded-lg
+      `}
+    >
+      <h2 
+        data-testid="add-new-board-form-title" 
+        className="
+          text-grey 
+          dark:text-white
+        "
+      >
+        {formTitle}
+      </h2>
+      <span 
+        data-testid="add-new-board-form-title-input" 
+        className="
+          flex 
+          flex-col 
+          mb-8 
+          mt-8
+        "
+      >
         <Input 
           testId="board-title-input"
           placeholder="e.g. Web Design"
@@ -102,12 +175,30 @@ const BoardForm = ({formTitle, boardName, boardColumns}: BoardFormProps)=> {
 
         />
       </span>
-      <div data-testid="add-new-board-form-columns-creator" className="flex flex-col">
-        <h4 className="text-grey-400 dark:text-white">Board Columns</h4>
+      <div 
+        data-testid="add-new-board-form-columns-creator" 
+        className="flex flex-col"
+      >
+        <h4 
+          className="
+            text-grey-400 
+            dark:text-white
+          "
+        >
+          Board Columns
+        </h4>
         <div ref={inputsContainer}>
           {emptyBoardColumns.map((column: BoardColumn, index)=> {
             return(
-              <div key={index} data-tesid="add-new-board-form-column" className="flex flex-row items-center">
+              <div 
+                key={index} 
+                data-tesid="add-new-board-form-column" 
+                className="
+                  flex 
+                  flex-row 
+                  items-center
+                "
+              >
                 <Input 
                   testId="column-name-input"
                   placeholder={column.placeholder}
@@ -116,16 +207,31 @@ const BoardForm = ({formTitle, boardName, boardColumns}: BoardFormProps)=> {
                   setValue={setBoardColumnName}
                   createResource={createResource}
                 />  
-                <IconButton sx={{color: '#fff', paddingRight: 0, paddingLeft: 2, paddingTop: 2}}>
+                <IconButton sx={
+                  {
+                    color: '#fff', 
+                    paddingRight: 0, 
+                    paddingLeft: 2, 
+                    paddingTop: 2
+                  }
+                }>
                   <img src="/images/icon-cross.svg" alt="" />
                 </IconButton>
               </div>
             )
           })}
         </div>
-        <PrimaryButton buttonText="+ Add New Column" color="white" handleClick={addNewColumnField}/>
+        <PrimaryButton 
+          buttonText="+ Add New Column" 
+          color="white" 
+          handleClick={addNewColumnField}
+        />
       </div>
-      <PrimaryButton buttonText="Create Board" color="primary" handleClick={submitForm}/>
+      <PrimaryButton 
+        buttonText="Create Board" 
+        color="primary" 
+        handleClick={submitForm}
+      />
     </form>
   )
 }
